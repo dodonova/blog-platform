2cb7f01bd239f86b2fb55ce276834f71c164cba6
Ekaterina Dodonova <e.a.dodonova@gmail.com>
2024-02-29 11:42:52 +0200

    Hide secrets settings.py

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e-cyd28ejdpt1ze8y%dt20)l(z61=u8igdpoz2u=yd3zn4erp-'
+# SECRET_KEY = 'django-insecure-e-cyd28ejdpt1ze8y%dt20)l(z61=u8igdpoz2u=yd3zn4erp-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
